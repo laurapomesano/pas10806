c6fcc4d8e5915191fdf7757d27cdc09f830fa7e0
laurapomesano <lpoaa@iscte-iul.pt>
2024-01-01 19:32:20 +0000

    Adicionar objeto

--- classes.py
+++ classes.py
@@ -3,3 +3,6 @@ class Carro:
         self.marca = marca
         self.modelo = modelo
         self.kms = kms
+
+    def __str__(self):
+        return f"{self.marca} {self.modelo} com {self.kms} kms"
---
+++ obj.py
@@ -0,0 +1,5 @@
+from classes import Carro
+
+c1 = Carro ("Volvo", "XC70", 1200000)
+
+print(c1)
