5f567337c4dc6dd04f8c45b84149cf8311f3cd60
laurapomesano <lpoaa@iscte-iul.pt>
2024-01-01 20:01:11 +0000

    Nova classe

--- classes.py
+++ classes.py
@@ -6,3 +6,12 @@ class Carro:
 
     def __str__(self):
         return f"{self.marca} {self.modelo} com {self.kms} kms"
+
+class livro:
+    def __init__(self, titulo, autor, ano):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+
+    def __str__(self):
+        return f"{self.titulo} - {self.autor} - {self.ano}"
